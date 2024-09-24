00809a8054f19b176ade18358bcb2ffe7bd3a65c
unknown <lucky7400547947@gmail.com>
2024-09-24 17:02:25 +0530

    tailwind css integrated

--- client/src/App.js
+++ client/src/App.js
@@ -3,20 +3,11 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
+    <div className=" bg-red-400">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1 class="text-3xl font-bold underline">
+          Hello world!
+        </h1>
       </header>
     </div>
   );
