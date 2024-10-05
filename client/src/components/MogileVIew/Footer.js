015c10345c197234860be081919974353c4fafbc
Parida <divyaranjan.parida@acsicorp.com>
2024-10-05 14:26:36 +0530

    mobile view navbar added

--- client/src/components/MogileVIew/Footer.js
+++ client/src/components/MogileVIew/Footer.js
@@ -3,8 +3,8 @@ import React from 'react'
 function Footer() {
     return (
         <div>
-            <div className="md:hidden mt-4">
-                <nav className="flex flex-col items-center space-y-4">
+            <div className="mt-4">
+                <nav className="flex flex-row items-center justify-evenly bottom-nav align-center">
                     <a href="#" className="text-gray-400 hover:text-white">Home</a>
                     <a href="#" className="text-gray-400 hover:text-white">Shop</a>
                     <a href="#" className="text-gray-400 hover:text-white">Contact</a>
